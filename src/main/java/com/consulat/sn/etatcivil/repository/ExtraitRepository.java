2489de1c4b1e4107648b91923791b4a4b3a0b696
sowoumar <mr.sowoumar@gmail.com>
2017-07-10 22:36:57 +0200

    amélioration requete recherche extrait de naissance

--- src/main/java/com/consulat/sn/etatcivil/repository/ExtraitRepository.java
+++ src/main/java/com/consulat/sn/etatcivil/repository/ExtraitRepository.java
@@ -1,10 +1,10 @@
 package com.consulat.sn.etatcivil.repository;
 
 import com.consulat.sn.etatcivil.domain.Extrait;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import org.springframework.data.jpa.repository.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,7 +14,7 @@ import java.util.List;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface ExtraitRepository extends JpaRepository<Extrait,Long> {
+public interface ExtraitRepository extends JpaRepository<Extrait, Long> {
 
     @Query("select extrait from Extrait extrait where extrait.agent.login = ?#{principal.username}")
     List<Extrait> findByAgentIsCurrentUser();
@@ -22,11 +22,10 @@ public interface ExtraitRepository extends JpaRepository<Extrait,Long> {
     public final static String FIND_DECLARATION_NAISSANCE_BY_CRITERIA = "SELECT d " +
         "FROM Extrait d JOIN FETCH d.enfant e " +
         "WHERE e.nom like :nom and e.prenom like :prenom " +
-        "and (d.id = :numeroRegistre or d.id is not null )" +
+        "and (d.numeroRegistre = :numeroRegistre or d.numeroRegistre is not null )" +
         "and (e.dateNaissance = :dateNaissance or e.dateNaissance is not null )";
 
     @Query(FIND_DECLARATION_NAISSANCE_BY_CRITERIA)
-    public List<Extrait> findExtraitByCriteria(@Param("numeroRegistre") Long numeroRegistre, @Param("nom") String nom,
+    public List<Extrait> findExtraitByCriteria(@Param("numeroRegistre") String numeroRegistre, @Param("nom") String nom,
                                                @Param("prenom") String prenom, @Param("dateNaissance") LocalDate dateNaissance);
-
 }
